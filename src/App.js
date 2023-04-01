ac269c636c2f822f3bf95fd62b1f037a77a1c108
Dan Moriarty <daniel.moriarty91@gmail.com>
2023-03-31 19:49:39 -0500

    Trying to figureout how to use tailwind to style a new character sheet. The hope is to use flex, grid, and whatever else might work to create a new sheet from scratch that is contained in one page.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="min-w-screen min-h-screen bg-slate-200">
+      <div className="grid grid-cols-3 gap-1">
+        <div className="basis-1/3 text-center">
+          Column One
+          <div className="h-80 w-3/4 bg-black">
+            <div className="h-1/4 bg-slate-400 border-2 border-black">
+              Section
+            </div>
+            <div className="h-1/4 bg-slate-400 border-2 border-black">
+              Section
+            </div>
+            <div className="h-1/4 bg-slate-400 border-2 border-black">
+              Section
+            </div>
+            <div className="h-1/4 bg-slate-400 border-2 border-black">
+              Section
+            </div>
+          </div>
+        </div>
+        <div className="basis-1/3 text-center">
+          Column Two
+          <div className="h-40 w-3/4 bg-black">
+            <div className="h-1/4 bg-slate-400 border-2 border-black">
+              Section
+            </div>
+            <div className="h-1/4 bg-slate-400 border-2 border-black">
+              Section
+            </div>
+            <div className="h-1/4 bg-slate-400 border-2 border-black">
+              Section
+            </div>
+            <div className="h-1/4 bg-slate-400 border-2 border-black">
+              Section
+            </div>
+          </div>
+        </div>
+        <div className="basis-1/3 text-center">
+          Column Three
+          <div className="h-20 w-3/4 bg-black">
+            <div className="h-1/4 bg-slate-400 border-2 border-black">
+              Section
+            </div>
+            <div className="h-1/4 bg-slate-400 border-2 border-black">
+              Section
+            </div>
+            <div className="h-1/4 bg-slate-400 border-2 border-black">
+              Section
+            </div>
+            <div className="h-1/4 bg-slate-400 border-2 border-black">
+              Section
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
